e68c7a8b51dc2b6d7917553181e5edaa5d6fd97b
Emerson <maria@maria-PC>
2013-09-22 18:44:04 -0300

    criação da conexão Mysql

---
+++ Sistema-de-Login/src/br/com/sistemadelogin/conexao/CriarConexao.java
@@ -0,0 +1,23 @@
+package br.com.sistemadelogin.conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class CriarConexao {
+//<!-- emmerson.henrique@gmail.com -->
+	
+	public static Connection getConexao() throws SQLException{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.print("conectou");
+			return DriverManager.getConnection("jdbc:mysql://localhost/sistemaLoginBd");
+		}  catch (SQLException e) {
+			throw new SQLException(e);
+		}catch (ClassNotFoundException e1) {
+			throw new SQLException(e1);
+			}	
+	
+	}
+	
+}
