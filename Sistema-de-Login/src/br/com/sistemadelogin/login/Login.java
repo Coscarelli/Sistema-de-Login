67e5de4d915b854a299b8dc30678d8e303eaf7bf
Emerson <emmerson.henrique@gmail.com>
2013-10-12 10:54:45 -0300

    10.Criação do modelo

---
+++ Sistema-de-Login/src/br/com/sistemadelogin/login/Login.java
@@ -0,0 +1,31 @@
+package br.com.sistemadelogin.login;
+
+public class Login {
+	
+	// emmerson.henrique@gmail.com 
+	
+	private int id;
+	private String usuario;
+	private String senha;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+
+}
